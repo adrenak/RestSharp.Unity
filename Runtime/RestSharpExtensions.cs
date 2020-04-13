265d406c17dc3582d69f9ad4d390ab6e0f22f8e1
Vatsal Ambastha <adrenak_@outlook.com>
2020-04-13 21:13:25 +0530

    feat: Remove 2.0 compatibility for simplicity

--- Runtime/RestSharpExtensions.cs
+++ Runtime/RestSharpExtensions.cs
@@ -1,9 +1,33 @@
 ﻿using System.Net;
 using System.Text;
 using System;
+using System.Threading.Tasks;
 
 namespace RestSharp {
-    public static partial class RestSharpExtensions {
+    public static class RestSharpExtensions {
+        /// <summary>
+        /// Executes the request asynchronously using a Task
+        /// </summary>
+        /// <param name="request">The request to be executed</param>
+        /// <returns>A Task for the response</returns>
+        public static Task<IRestResponse> ExecuteAsync(this RestClient client, RestRequest request) {
+            var task = new TaskCompletionSource<IRestResponse>();
+            client.ExecuteAsync(request, response => task.SetResult(response));
+            return task.Task;
+        }
+
+        /// <summary>
+        /// Executes the request asynchronously using a Task
+        /// </summary>
+        /// <param name="request">The request to be executed</param>
+        /// <param name="handle">A handle to allow aborting the execution</param>
+        /// <returns>A Task for the response</returns>
+        public static Task<IRestResponse> ExecuteAsync(this RestClient client, RestRequest request, out RestRequestAsyncHandle handle) {
+            var task = new TaskCompletionSource<IRestResponse>();
+            handle = client.ExecuteAsync(request, response => task.SetResult(response));
+            return task.Task;
+        }
+
         /// <summary>
         /// Returns if the Status Code implies success 
         /// </summary>
@@ -18,11 +42,11 @@ namespace RestSharp {
         }
 
         /// <summary>
-        /// Returns if the response was successful 
+        /// Returns if the response was successful.
         /// </summary>
         /// <param name="response"></param>
         /// <returns></returns>
-        public static bool IsSuccess(this IRestResponse response) {
+        public static bool IsSuccessful(this IRestResponse response) {
             return response.StatusCode.IsSuccess() && response.ResponseStatus == ResponseStatus.Completed;
         }
 
@@ -30,7 +54,7 @@ namespace RestSharp {
         /// Returns a <see cref="RestSharpException"/> exception if the response was not successful
         /// </summary>
         public static Exception GetException(this IRestResponse response) {
-            if (response.IsSuccess())
+            if (response.IsSuccessful())
                 return null;
             else
                 return new RestSharpException(response.StatusCode, response.ResponseUri, response.Content, response.GetError(), response.ErrorException);
@@ -40,7 +64,7 @@ namespace RestSharp {
         /// Returns the error string, if the response was not successful. 
         /// </summary>
         public static string GetError(this IRestResponse response) {
-            if (response.IsSuccess())
+            if (response.IsSuccessful())
                 return string.Empty;
 
             var sb = new StringBuilder();
